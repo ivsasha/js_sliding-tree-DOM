b31c05759438ce96b10ee0faf24c8df5979ced4d
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-24 23:07:05 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,7 @@ document.querySelectorAll('li').forEach((li) => {
 
     span.textContent = textNode.textContent.trim();
     textNode.textContent = '';
-    li.insertBefore(span, li.firstChild);
+    li.replaceChild(span, li.firstChild);
   });
 });
 
